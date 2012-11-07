ba1be97e38c5aed4618fddf6970258e961e0240d
Niklas Baumstark <niklas.baumstark@gmail.com>
2012-11-07 18:19:39 +0100

    pflichtenheft: add BibTeX, do some styling, update Makefiles, work on Zielbestimmung

--- pflichtenheft/make.cmd
+++ pflichtenheft/make.cmd
@@ -1,4 +1,6 @@
 pdflatex pflichtenheft.tex
 makeglossaries pflichtenheft
+bibtex pflichtenheft
+pdflatex pflichtenheft.tex
 pdflatex pflichtenheft.tex
 del *.class *.html *.log *.aux *.out *.glo *.glg *.gls *.ist *.xdy *.1 *.toc
