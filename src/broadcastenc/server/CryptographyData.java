c1da7f1b010b16ea725b04957f0b1dd4e58716d2
Tobias Gräf <tobias_graef111@yahoo.de>
2012-11-28 21:09:55 +0100

    changes in cryptographyData

--- src/broadcastenc/server/CryptographyData.java
+++ src/broadcastenc/server/CryptographyData.java
@@ -2,10 +2,11 @@ package broadcastenc.server;
 
 /** Contains the data which is changed by Controller and presented by View
  */
-public class CryptographyData {
+public class CryptographyData implements Serializable {
 
 	/** Saves the mapping of Users to PrivateKeys
 	*/
 	private HashMap<User, PrivateKey> users;
+	private ArrayList<User> revoked;
     
 }
