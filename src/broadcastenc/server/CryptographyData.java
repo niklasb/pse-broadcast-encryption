fecc531b6ef116172de99daf76267e4b78b2dbcd
Tobias Gräf <tobias_graef111@yahoo.de>
2012-11-28 18:08:03 +0100

    changed name from View to Shell and from Model to CryprographyData

---
+++ src/broadcastenc/server/CryptographyData.java
@@ -0,0 +1,11 @@
+package broadcastenc.server;
+
+/** Contains the data which is changed by Controller and presented by View
+ */
+public class CryptographyData {
+
+	/** Saves the mapping of Users to PrivateKeys
+	*/
+	private HashMap<User, PrivateKey> users;
+    
+}
---
+++ src/broadcastenc/server/Shell.java
@@ -0,0 +1,17 @@
+package broadcastenc.server;
+
+import java.io.PrintStream;
+
+import broadcastenc.util.CommandLineInterface;
+
+/** Deals with interaction of user and gets the arguments from the command line
+ */
+public class Shell extends CommandLineInterface {
+
+	@Override
+	protected void start(String[] args) throws Exit {
+		// TODO Auto-generated method stub
+		
+	}
+    
+}
