50dda26954a4ea91064df6ee576f783e8e618872
Niklas Baumstark <niklas.baumstark@gmail.com>
2012-11-29 22:21:37 +0100

    fix indentation

--- src/broadcastenc/crypto/BroadcastSchemeClient.java
+++ src/broadcastenc/crypto/BroadcastSchemeClient.java
@@ -3,6 +3,6 @@ package broadcastenc.crypto;
 class PersonalKey { }
 
 public abstract class BroadcastSchemeClient<T, U> {
-  public BroadcastSchemeClient(PersonalKey key) { }
-  protected abstract Share<T, U> getPrivateShare();
+    public BroadcastSchemeClient(PersonalKey key) { }
+    protected abstract Share<T, U> getPrivateShare();
 }
--- src/broadcastenc/crypto/Share.java
+++ src/broadcastenc/crypto/Share.java
@@ -6,8 +6,8 @@ class InsufficientInformationException extends Exception {}
  * @param U phantom type to ensure that two Share<T, U> are always compatible
  */
 public interface Share<T, U> {
-  public Share<T, U> update(Share<T, U> other);
-  public T restore() throws InsufficientInformationException;
-  public byte[] pack();
+    public Share<T, U> update(Share<T, U> other);
+    public T restore() throws InsufficientInformationException;
+    public byte[] pack();
 }
 
