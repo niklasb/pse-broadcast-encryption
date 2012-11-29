1bcb22835e2f5cec96594bd844bd70465e74ef66
Amrei <amreiloose@googlemail.com>
2012-11-29 09:43:18 +0100

    added methods mapAddUser and isRevoked

--- src/broadcastenc/server/CryptographyData.java
+++ src/broadcastenc/server/CryptographyData.java
@@ -4,9 +4,27 @@ package broadcastenc.server;
  */
 public class CryptographyData implements Serializable {
 
-	/** Saves the mapping of Users to PrivateKeys
-	*/
-	private HashMap<User, PrivateKey> users;
-	private ArrayList<User> revoked;
+    /** Saves the mapping of Users to PrivateKeys
+     */
+    private HashMap<User, PrivateKey> users;
+    private ArrayList<User> revoked;
     
+    /**
+     * Adds a new user and its private key to the map
+     * @param user The user that is added.
+     * @param privateKey The assigned private key.
+     * @return true if the user was added, false else
+     */
+    public boolean mapAddUser(User user, PrivateKey privateKey) {
+        
+    }
+    
+    /**
+     * Checks if the given user is on the revoked list
+     * @param user the user that is checked
+     * @return true if the user is revoked, false if not
+     */
+    public boolean isRevoked(User user) {
+        
+    }
 }
