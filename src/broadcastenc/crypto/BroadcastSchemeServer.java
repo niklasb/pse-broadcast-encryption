c0e0f79dc70598d6395dbcf04c1f4b74d1be2469
Niklas Baumstark <niklas.baumstark@gmail.com>
2012-11-30 19:50:16 +0100

    update crypto

--- src/broadcastenc/crypto/BroadcastSchemeServer.java
+++ src/broadcastenc/crypto/BroadcastSchemeServer.java
@@ -2,9 +2,12 @@ package broadcastenc.crypto;
 
 import java.util.Collection;
 
-class Identity { }
-
-public interface BroadcastSchemeServer<T, U extends Share<T>> {
-    public void revoke(Identity i);
+/** The server side of a broadcast encryption scheme.
+ * @param T The type of the secret
+ * @param U The type of the shares
+ * @param V The type of the identities
+ */
+public interface BroadcastSchemeServer<T, U extends Share<T>, V> {
+    public void revoke(V i);
     public Collection<U> getShares();
 }
--- src/broadcastenc/crypto/NaorPinkasBroadcastSchemeServer.java
+++ src/broadcastenc/crypto/NaorPinkasBroadcastSchemeServer.java
@@ -4,9 +4,14 @@ import java.math.BigInteger;
 import java.util.Collection;
 
 public class NaorPinkasBroadcastSchemeServer
-        implements BroadcastSchemeServer<BigInteger, NaorPinkasShare> {
-    public void revoke(Identity x) { }
-    public Collection<NaorPinkasShare> getShares() {
-        return null;
+      implements BroadcastSchemeServer<BigInteger,
+                                       NaorPinkasShare,
+                                       NaorPinkasBroadcastSchemeServer.Identity> {
+    class Identity {
+      private Identity(BigInteger id) {}
+      public BigInteger getId() { return null; }
     }
+
+    public void revoke(Identity x) { }
+    public Collection<NaorPinkasShare> getShares() { return null; }
 }
