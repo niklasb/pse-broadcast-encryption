d7326da9fe9efbe7e8d75f1a4cb0fe7752c8fcc2
Mohammed Abu Jayyab <mohamed_20_90@hotmail.com>
2012-12-02 22:01:13 +0200

    add client package

---
+++ src/broadcastenc/client/KeyChoiceActivity.java
@@ -0,0 +1,21 @@
+package broadcastenc.client;
+
+import java.io.File;
+
+
+/**
+ * This activity lets a user choose an encryption key file 
+ * which is then sent to the server for authentication.
+ */
+public class KeyChoiceActivity {
+
+	
+	/**
+	 * Chooses a key file
+	 * @param file the key file
+	 */
+	public void chooseKeyFile(File file) {
+		
+	}
+	
+}
