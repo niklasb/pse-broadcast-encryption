0ef48ff7478ca51c519f01e6389c570bba38b1f9
Niklas Baumstark <niklas.baumstark@gmail.com>
2012-12-03 16:49:53 +0100

    move design document

--- entwurf/make.cmd
+++
@@ -1,6 +0,0 @@
-pdflatex pflichtenheft.tex
-makeglossaries pflichtenheft
-bibtex pflichtenheft
-pdflatex pflichtenheft.tex
-pdflatex pflichtenheft.tex
-del *.class *.html *.log *.aux *.out *.glo *.glg *.gls *.ist *.xdy *.1 *.toc *.bbl *.blg
