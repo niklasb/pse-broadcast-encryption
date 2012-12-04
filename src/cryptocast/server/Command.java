5fbbd2d583fff3c4ec3702aec86754fc3a4fdd19
Tobias Gräf <tobias_graef111@yahoo.de>
2012-12-04 20:51:44 +0100

    changes in command

--- src/cryptocast/server/Command.java
+++ src/cryptocast/server/Command.java
@@ -1,14 +1,16 @@
 package cryptocast.server;
 
-import java.util.Collection;
-
+/**
+ * This class represents a command and saves all legal commands.
+ */
 public class Command {
     /**
      * This Collection contains all possible commands.
      */
-    public static final Command[] commands = 
+    protected static final Command[] commands = 
             { new Command("addUser", "addUser 'name' tries to add a user to the stream", "")};
     
+    //argument description? can this class check arguments of a command?
     private String cmd;
     private String shortDescription;
     private String longDescription;
