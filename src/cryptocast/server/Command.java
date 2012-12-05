5e875bc4a9c7390c74486f407d7af9698f466172
Niklas Baumstark <niklas.baumstark@gmail.com>
2012-12-05 20:21:50 +0100

    make enum out of command

--- src/cryptocast/server/Command.java
+++ src/cryptocast/server/Command.java
@@ -1,32 +1,45 @@
 package cryptocast.server;
 
+import java.util.Map;
+
 /**
- * This class represents a command and saves all legal commands.
+ * This enum represents the commands available in the
  */
-public final class Command {
-    /**
-     * This Collection contains all possible commands.
-     */
-    protected static final Command[] COMMANDS = 
-            { new Command("addUser", "addUser <name>", "addUser 'name' tries to add a user to the stream", "")};
-    
-    //argument description? syntax
-    private String cmd;
+public enum Command {
+    ADD_USER    ("addUser",       "addUser <name>",
+                     "adds a new user to the group of recipients"),
+    REVOKE_USER ("revokeUser",    "revokeUser <name>",
+                     "revokes a user"),
+    ;
+
+    public static Map<String, Command> commands;
+    static {
+        for (Command cmd : Command.class.getEnumConstants())
+            commands.put(cmd.getName(), cmd);
+    }
+
+    private String name;
     private String syntax;
-    private String shortDescription;
-    private String longDescription;
-    
+    private String description;
+
     /**
      * Creates a new Command with the given values
-     * @param cmd 
-     * @param description
+     * @param name The name of the command
+     * @param syntax The syntax of the command
+     * @param description A short description of the functionality
      */
-    private Command(String cmd, String syntax, String shortDescription, String longDescription) {
-        super();
-        this.cmd = cmd;
+    private Command(String name, String syntax, String description) {
+        this.name = name;
         this.syntax = syntax;
-        this.longDescription = longDescription;
-        this.shortDescription = shortDescription;
-    } 
-    
+        this.description = description;
+    }
+
+    /** @return The command name */
+    public String getName() { return name; }
+
+    /** @return The command syntax */
+    public String getSyntax() { return syntax; }
+
+    /** @return The command description */
+    public String getDescription() { return description; }
 }
