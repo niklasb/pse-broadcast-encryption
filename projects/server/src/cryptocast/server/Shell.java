1dfe7c52d71867bde24b567c394bd37cb1a94b33
Niklas Baumstark <niklas.baumstark@gmail.com>
2012-12-19 15:25:50 +0100

    implement shell help

--- projects/server/src/cryptocast/server/Shell.java
+++ projects/server/src/cryptocast/server/Shell.java
@@ -3,6 +3,7 @@ package cryptocast.server;
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.SortedMap;
+import java.util.TreeMap;
 
 import cryptocast.util.InteractiveCommandLineInterface;
 
@@ -11,6 +12,29 @@ import cryptocast.util.InteractiveCommandLineInterface;
  * @param <ID> The type of the user identities.
  */
 public class Shell<ID> extends InteractiveCommandLineInterface {
+    private static ShellCommand commands[] = new ShellCommand[] {
+        new ShellCommand("help",
+                         "[<command>]",
+                         "Shows the command line help",
+                         null),
+        new ShellCommand("addUser",
+                         "<name>",
+                         "Adds a new user to the group of recipients",
+                         null),
+        new ShellCommand("revokeUser",
+                         "<name>",
+                         "Revokes a user",
+                         null),
+    };
+
+    private static SortedMap<String, ShellCommand> commandsByName = 
+            new TreeMap<String, ShellCommand>();
+    static {
+        for (ShellCommand cmd : commands) {
+            commandsByName.put(cmd.getName(), cmd);
+        }
+    }
+
     private Controller<ID> control;
 
     /**
@@ -24,24 +48,41 @@ public class Shell<ID> extends InteractiveCommandLineInterface {
     }
 
     @Override
-    protected void performCommand(String cmdStr, String[] args) throws CommandError {
-        ShellCommand cmd = ShellCommand.commands.get(cmdStr);
+    protected void performCommand(String cmdName, String[] args) throws CommandError {
+        ShellCommand cmd = commandsByName.get(cmdName);
         if (cmd == null) {
             error("No such command! Type `help' to get an overview of the available commands.");
         }
-        switch (cmd) {
-        case HELP: help(); break;
-        }
+        if (cmd.getName() == "help") { help(cmd, args); }
     }
 
     /**
      * Prints all commands this shell can perform with information about how to use them.
      */
-    private void help() {
-        println("Available commands:");
-        println();
-        for (ShellCommand cmd : ShellCommand.commands.values()) {
-            printf("%-12s %s\n", cmd.getName(), cmd.getShortDesc());
+    private void help(ShellCommand cmd, String[] args) throws CommandError {
+        if (args.length > 1) {
+            commandSyntaxError(cmd);
         }
+        if (args.length == 1) {
+            ShellCommand helpCmd = commandsByName.get(args[0]);
+            if (helpCmd == null) {
+                error("No such command: `%s'", args[0]);
+            }
+            printf("Usage: %s %s\n", helpCmd.getName(), helpCmd.getSyntax());
+            println();
+            println(helpCmd.getLongDesc());
+        } else {
+            println("Available commands:");
+            println();
+            for (ShellCommand c : commandsByName.values()) {
+                printf("%-12s %s\n", c.getName(), c.getShortDesc());
+            }
+            println();
+            println("Use `help <cmd>' to get more information about a specific command");
+        }
+    }
+    
+    private void commandSyntaxError(ShellCommand cmd) throws CommandError {
+        error("Invalid Syntax! Usage: %s %s", cmd.getName(), cmd.getSyntax());
     }
 }
--- projects/server/src/cryptocast/server/ShellCommand.java
+++ projects/server/src/cryptocast/server/ShellCommand.java
@@ -1,37 +1,9 @@
 package cryptocast.server;
 
-import java.util.SortedMap;
-import java.util.TreeMap;
-
 /**
  * Represents the commands available in the shell.
  */
-public enum ShellCommand {
-    HELP (
-            "help",
-            "help",
-            "Shows the command line help",
-            null),
-    ADD_USER (
-            "addUser",
-            "addUser <name>",
-            "Adds a new user to the group of recipients",
-            null),
-    REVOKE_USER (
-            "revokeUser",
-            "revokeUser <name>",
-            "Revokes a user",
-            null),
-    ;
-
-    public static SortedMap<String, ShellCommand> commands = 
-            new TreeMap<String, ShellCommand>();
-    static {
-        for (ShellCommand cmd : ShellCommand.class.getEnumConstants()) {
-            commands.put(cmd.getName(), cmd);
-        }
-    }
-
+public class ShellCommand {
     private String name, syntax, shortDesc, longDesc;
 
     /**
@@ -40,7 +12,7 @@ public enum ShellCommand {
      * @param syntax The syntax of the command
      * @param description A short description of the functionality
      */
-    private ShellCommand(String name, String syntax, String shortDesc, String longDesc) {
+    public ShellCommand(String name, String syntax, String shortDesc, String longDesc) {
         this.name = name;
         this.syntax = syntax;
         this.shortDesc = shortDesc;
