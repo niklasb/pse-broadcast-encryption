054129166675b9df41f5bd7d4e4d24b5d081ad72
Niklas Baumstark <niklas.baumstark@gmail.com>
2012-12-20 20:58:03 +0100

    add unit test for Generator

---
+++ projects/common/junit/cryptocast/util/test/TestGenerator.java
@@ -0,0 +1,26 @@
+package cryptocast.util.test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import org.mockito.Mockito;
+import org.junit.Test;
+
+import cryptocast.util.Generator;
+
+public class TestGenerator {
+
+	@Test
+	public void getRangeWorks() {
+		@SuppressWarnings("unchecked")
+        Generator<Integer> gen = mock(Generator.class, Mockito.CALLS_REAL_METHODS);
+		doReturn(5).when(gen).get(0);
+		doReturn(4).when(gen).get(1);
+		doReturn(3).when(gen).get(2);
+		doReturn(2).when(gen).get(3);
+		doReturn(1).when(gen).get(4);
+		doReturn(0).when(gen).get(5);
+		assertArrayEquals(new Integer[] { 4, 3, 2 }, 
+		                  gen.getRange(1, 4));
+	}
+
+}
