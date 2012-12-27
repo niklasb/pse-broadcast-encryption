2bef4d003b52973db26183fcfe0efee0481904cb
Tobias Gräf <tobias_graef111@yahoo.de>
2012-12-27 17:42:09 +0100

    added test for MainActivity in client

--- projects/client_tests/src/cryptocast/client/test/TestMainActivity.java
+++ projects/client_tests/src/cryptocast/client/test/TestMainActivity.java
@@ -2,15 +2,31 @@ package cryptocast.client.test;
 
 import cryptocast.client.MainActivity;
 import android.app.Activity;
+import android.test.ActivityInstrumentationTestCase2;
 import android.test.ActivityTestCase;
 
-public class TestMainActivity extends ActivityTestCase {
+public class TestMainActivity extends ActivityInstrumentationTestCase2<MainActivity> {
+    MainActivity mActivity;
     
+    public TestMainActivity() {
+        super("cryptocast.client", MainActivity.class);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+      super.setUp();
+      
+      mActivity = getActivity();
+    }
+    
+    public void testPreConditions() {
+        assertTrue(mActivity != null);
+    }
+
     public void testServerNameSaving() {
-        MainActivity mActivity = new MainActivity();
         String expected = "serverName";
         mActivity.storeServerName(expected);
-        //String actual = mActivity.loadServerName();
-        //assertEquals(expected, actual);
+        String actual = mActivity.loadServerName();
+        assertEquals(expected, actual);
     }
 }
