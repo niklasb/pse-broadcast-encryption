b2f9ec51d2521966cf6e396a15e9bb8be80cb824
Tobias Gräf <tobias_graef111@yahoo.de>
2012-12-28 21:17:00 +0100

    added ideas for ServerHistory

--- projects/client/src/cryptocast/client/ServerHistory.java
+++ projects/client/src/cryptocast/client/ServerHistory.java
@@ -11,10 +11,14 @@ import java.util.Map;
  */
 public class ServerHistory implements Serializable {
     private static final long serialVersionUID = 140551767140374605L;
+    private final String FILENAME = "serverHistory";
     
     // A map for servers and their corresponding key files
     private Map<String, File> servers;
-
+    
+    //server history should only be created if it could not be loaded from storage
+    private ServerHistory() { }
+    
     /**
      * @return The servers
      */
@@ -25,5 +29,22 @@ public class ServerHistory implements Serializable {
      * @param hostname The server's hostname
      * @param keyfile The keyfile the user has chosen for this server.
      */
-    public void addServer(String hostname, File keyfile) { }
+    public void addServer(String hostname, File keyfile) { 
+        
+    }
+    
+    /**
+     * Loads an instance of serverHistory with the name specified by FILENAME 
+     * from the app's internal storage.
+     * @return The instance of serverHistory loaded or if a new instance of serverHistory
+     * if it does not exist.
+     */
+    public static ServerHistory getServerHistory() {
+        return null;
+    }
+    
+    //saves this instance at the app's internal storage using FILENAME as name of the file
+    private void saveServerHistory() {
+        
+    }
 }
