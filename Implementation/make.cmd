d991615fdbc82b1dfb92fa056dc78750e6cbf1f5
Tobias Gräf <tobias_graef111@yahoo.de>
2013-01-20 11:53:51 +0100

    added some batch scripts and adjustet folder Implementation

--- Implementation/make.cmd
+++ Implementation/make.cmd
@@ -1,3 +1,3 @@
-pdflatex design_document.tex
-pdflatex design_document.tex
+pdflatex implementation_document.tex
+pdflatex implementation_document.tex
 del *.class *.html *.log *.aux *.out *.glo *.glg *.gls *.ist *.xdy *.1 *.toc *.bbl *.blg
---
+++ bin/clientBatch.bat
@@ -0,0 +1,8 @@
+@echo off
+setLocal EnableDelayedExpansion
+cd "%~dp0/.."
+set CLASSPATH=
+FOR %%F IN (3rd_party/*.jar) DO (
+  set CLASSPATH=!CLASSPATH!;3rd_party/%%F
+)
+java -cp "%CLASSPATH%;projects/server/bin/" cryptocast.server.programs.Client
